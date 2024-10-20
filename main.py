cbed97488ddebc4bc1dde27439ab6d3ae5c51395
w7b <pessoal.gabriel@gmail.com>
2024-10-20 10:20:20 -0300

    Version Unstable

--- main.py
+++ main.py
@@ -39,11 +39,48 @@ async def read_acc(path):
         return accounts 
 
 async def global_login(url, user, password):
-    data = {
-        'username_field': user,
-        'password_field': password
-    }
-#Retornando 200 mesmo não dando login, verificar isso
+    async with aiohttp.ClientSession() as session:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
+        }
+
+        async with session.get(url, headers=headers) as initial_response:
+            if initial_response == 200:
+                soup = BeautifulSoup(await initial_response.text(), 'html.parser')
+
+                csrf_token = soup.find('input', {'name': 'csrf_token'})
+                if csrf_token:
+                    csrf_token = csrf_token['value']
+                else:
+                    csrf_token = ''
+
+                data = {
+                    'username': user,
+                    'password': password,
+                    #'csrf_token': csrf_token
+                }
+
+                async with session.post(url, data=data, headers=headers) as login_response:
+                    if login_response.status == 200:
+                        page_content = await login_response.text()
+                        print(page_content)
+                        if "dashboard" in page_content.lower():
+                            print(f"\nLogin bem-sucedido para o usuário {user}")
+                        else:
+                            print(f"\nFalha no login para o usuário {user}")
+                    elif login_response.status == 401:
+                        print(f"\nErro 401: Login não autorizado para o usuário {user}. Verifique as credenciais.")
+                    else:
+                        print(f"\nFalha ao tentar logar. Status HTTP: {login_response.status}")
+            else:
+                print(f"\nErro ao acessar o site. Status HTTP: {initial_response.status}") ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR ##VERIFICAR 
+
+            
+
+    
 
 async def s_menu1():
     while True:
